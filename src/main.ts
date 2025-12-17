aba2284ef50761f3fc35fba2b2ad75279685226c
gorushkin <gorushkin@users.noreply.github.com>
2025-12-17 12:07:48 +0300

    feat: restructure form layout and update sign-up page input names for consistency

--- src/main.ts
+++ src/main.ts
@@ -38,6 +38,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const app = document.getElementById("app");
 
   if (app) {
-    app.innerHTML = pages.chat();
+    app.innerHTML = pages.login();
   }
 });
