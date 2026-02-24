18b1fefa4bff823123d70cd6fbdae403a1f325c9
gorushkin <gorushkin@users.noreply.github.com>
2026-02-24 18:17:36 +0300

    feat: update chat header to use ImageWithDefault for avatar handling

--- src/widgets/chat-window/chat-header/chat-header.ts
+++ src/widgets/chat-window/chat-header/chat-header.ts
@@ -1,5 +1,5 @@
 import type { ChatData } from "../../../api";
-import { Image } from "../../../components/image";
+import { ImageWithDefault } from "../../../components/image/image";
 import { Block, type PropsAndChildren } from "../../../libs/block";
 import { withCurrentChat } from "../../../libs/connect";
 import { ChatSettingsButton } from "../chatSettingsButton";
@@ -13,8 +13,8 @@ type ChatHeaderProps = PropsAndChildren & {
 
 class ChatHeader extends Block {
   constructor(propsAndChildren: ChatHeaderProps) {
-    const avatar = new Image({
-      src: "/profile_default.svg",
+    const avatar = new ImageWithDefault({
+      src: propsAndChildren.currentChat?.avatar ?? "",
       alt: "Chat avatar",
       className: "chat-header__avatar-img",
     });
@@ -28,6 +28,24 @@ class ChatHeader extends Block {
       },
       true,
     );
+  }
+
+  componentDidUpdate(
+    oldProps: ChatHeaderProps,
+    newProps: ChatHeaderProps,
+  ): boolean {
+    const isChanged =
+      oldProps.currentChat?.avatar !== newProps.currentChat?.avatar;
+
+    if (isChanged) {
+      this.children.avatar = new ImageWithDefault({
+        src: newProps.currentChat?.avatar ?? "",
+        alt: "Chat avatar",
+        className: "chat-header__avatar-img",
+      });
+    }
+
+    return super.componentDidUpdate(oldProps, newProps);
   }
 }
 
